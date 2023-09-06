51c4e8b66ce7807565c6152c99331798d5abb14a
Alexei Korol <alexkorol@users.noreply.github.com>
2023-09-07 00:22:03 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,105 +1,82 @@
-function extractDataFromCurrentImage() {
-    const image = document.querySelector('img.modalImage');
-    const userElement = document.querySelector('p.ml-4');
-    const parametersElement = document.querySelector('div.miniScrollbar');
-    const imagePromptElement = document.querySelector('div.miniScrollbar > div.grid');
+async function extractData() {
+  const imgElement = document.querySelector('img.modalImage');
+  const promptElement = document.querySelector('#modalPrompt');
+  const paramsElement = document.querySelector('code');
 
-    const parameters = parametersElement ? parametersElement.innerText : 'N/A';
-    const parsedParameters = parameters
-        .split('--')
-        .filter(Boolean)
-        .map(param => {
-            const [key, value] = param.trim().split(' ');
-            return { [key]: value || true };
-        });
-    
-    const currentData = {
-        imageUrl: image.src,
-        prompt: image.alt,
-        user: userElement ? userElement.innerText : 'N/A',
-        parameters: parsedParameters
-    };
+  let url = imgElement ? imgElement.src : 'N/A';
+  let prompt = promptElement ? promptElement.innerText : 'N/A';
+  let params = paramsElement ? paramsElement.innerText : 'N/A';
 
-    if (imagePromptElement) {
-        currentData.imagePrompt = 'yes';
-    }
+  if (params === 'No arguments') params = '';
 
-    return currentData;
+  prompt = prompt.replace(/\n+/g, ' ').trim();
+  if (params !== '') {
+    prompt = `${prompt} ${params}`;
+  }
+
+  return {
+    url,
+    prompt
+  };
 }
 
-function waitForImageChange(currentImageUrl) {
-    return new Promise((resolve) => {
-        const checkInterval = setInterval(() => {
-            const newImageUrl = document.querySelector('img.modalImage').src;
-            if (newImageUrl !== currentImageUrl) {
-                clearInterval(checkInterval);
-                resolve();
-            }
-        }, 500);
-    });
-}
-
-function navigateToNextImage() {
-    const currentImageUrl = document.querySelector('img.modalImage').src;
-    const nextArrow = document.querySelector('#jobPage > button.hidden.fixed.top-1\\/2.right-10.z-50.justify-center.items-center.w-14.h-14.text-\\[\\#D6DDF0\\].hover\\:bg-darkBlue-500\\/80.active\\:bg-uiBlue-500\\/60.rounded-full.outline-2.active\\:outline-\\[\\#7898D7\\].active\\:outline.outline-offset-2.disabled\\:opacity-0.transition-colors.-translate-y-1\\/2.lg\\:flex');
-    nextArrow.click();
-    return waitForImageChange(currentImageUrl);
+function clickNextButton() {
+  return new Promise(resolve => {
+    const button = document.querySelector('button[title="Next"]');
+    if (button) button.click();
+    setTimeout(resolve, 250);
+  });
 }
 
 function displayData(data) {
-    const textarea = document.createElement('textarea');
-    const copyButton = document.createElement('button');
-    const clearButton = document.createElement('button');
+  const textarea = document.createElement('textarea');
+  const copyButton = document.createElement('button');
+  const clearButton = document.createElement('button');
 
-    // Textarea setup
-    textarea.style.width = '80%';
-    textarea.style.height = '200px';
-    textarea.style.position = 'fixed';
-    textarea.style.top = '10%';
-    textarea.style.left = '10%';
-    textarea.style.zIndex = '9999';
-    textarea.innerText = JSON.stringify(data, null, 2);
+  textarea.style.zIndex = "10000";
+  textarea.style.position = "fixed";
+  textarea.style.top = "20px";
+  textarea.style.left = "20px";
+  textarea.style.width = "300px";
+  textarea.style.height = "150px";
+  
+  // Adding a trailing comma to the output
+  textarea.value = data.map(d => JSON.stringify(d, null, 2)).join(',\n') + ',';
+  
+  copyButton.innerHTML = 'Copy';
+  copyButton.style.zIndex = "10001";
+  copyButton.style.position = "fixed";
+  copyButton.style.top = "180px";
+  copyButton.style.left = "20px";
 
-    // Copy Button setup
-    copyButton.innerText = 'Copy Data';
-    copyButton.style.position = 'fixed';
-    copyButton.style.top = '5%';
-    copyButton.style.left = '10%';
-    copyButton.style.zIndex = '10000';
-    copyButton.onclick = function() {
-        textarea.select();
-        document.execCommand('copy');
-        alert('Data copied to clipboard!');
-    };
+  clearButton.innerHTML = 'Clear';
+  clearButton.style.zIndex = "10001";
+  clearButton.style.position = "fixed";
+  clearButton.style.top = "180px";
+  clearButton.style.left = "80px";
 
-    // Clear Button setup
-    clearButton.innerText = 'Clear Data';
-    clearButton.style.position = 'fixed';
-    clearButton.style.top = '5%';
-    clearButton.style.left = '20%';
-    clearButton.style.zIndex = '10000';
-    clearButton.onclick = function() {
-        textarea.innerText = '';
-    };
+  copyButton.addEventListener('click', () => {
+    textarea.select();
+    document.execCommand('copy');
+  });
 
-    document.body.appendChild(textarea);
-    document.body.appendChild(copyButton);
-    document.body.appendChild(clearButton);
+  clearButton.addEventListener('click', () => {
+    textarea.value = '';
+  });
+
+  document.body.appendChild(textarea);
+  document.body.appendChild(copyButton);
+  document.body.appendChild(clearButton);
 }
 
-async function scrapeImages() {
-    const data = [];
-    const numberOfImages = 30; // Adjust as needed
-
-    for (let i = 0; i < numberOfImages; i++) {
-        const currentImageData = extractDataFromCurrentImage();
-        data.push(currentImageData);
-        if (i < numberOfImages - 1) {
-            await navigateToNextImage();
-        }
-    }
-
-    displayData(data);
-}
-
-scrapeImages();
+(async function main() {
+  const allData = [];
+  
+  for (let i = 0; i < 30; i++) {
+    const data = await extractData();
+    allData.push(data);
+    await clickNextButton();
+  }
+  
+  displayData(allData);
+})();
